7af6d9638961f3d247e311eb6f2f4603fed757a1
PhTrias <vettrias@outlook.com>
2023-01-13 18:38:14 -0300

    Feat: Add models spec

--- spec/models/launch_spec.rb
+++ spec/models/launch_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe Launch, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Launch do
+  context '#create' do
+    it 'create launch' do
+      launch =
+        Launch.create!(name: 'foo', slug: 'bar')
+      expect(launch).to be_present
+    end
+  end
+
+  context '#update' do
+    let!(:launch) do
+      create(:launch, name: 'foo', slug: 'bar')
+    end
+
+    it 'update values' do
+      launch.update(name: 'BUZZ')
+
+      expect(launch.reload.name).to eq('BUZZ')
+    end
+  end
 end
