4d15b04c07cd956f0be7e96e10f3ab2966b68183
PhTrias <vettrias@outlook.com>
2023-01-13 10:37:59 -0300

    Clean: Remove useless files

--- db/migrate/20230105232630_add_hash_and_array_colums_to_launch.rb
+++
@@ -1,9 +0,0 @@
-class AddHashAndArrayColumsToLaunch < ActiveRecord::Migration[7.0]
-  def change
-    add_column :launches, :status, :text, hash: true, default: '{}'
-    add_column :launches, :launch_service_provider, :text, hash: true, default: '{}'
-    add_column :launches, :rocket, :text, hash: true, default: '{}'
-    add_column :launches, :pad, :text, hash: true, default: '{}'
-    add_column :launches, :program, :text, hash: true, default: '[]'
-  end
-end
